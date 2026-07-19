37b9108b0e7943b94bde4468e48b4e3ca2c6ad5e
Chaitanya1819 <chaitanyabejjanki18@gmail.com>
2026-07-19 00:10:43 -0400

    added front end

---
+++ shopkart-ui/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <h1>ShopKart</h1>
+        <p>Welcome to our store</p>
+      </div>
+  );
+}
+
+export default App;
