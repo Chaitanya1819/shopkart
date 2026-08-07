a19bdb2502d191aa355bacfe22cfed7e8b04b928
Chaitanya1819 <chaitanyabejjanki18@gmail.com>
2026-08-07 18:00:05 -0400

    Add Cart page connected to Cart Service

--- shopkart-ui/src/App.js
+++ shopkart-ui/src/App.js
@@ -1,7 +1,11 @@
+
+
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Products from './pages/Products';
+import Cart from './pages/Cart';
+// import Orders from './pages/Orders';
 
 function App() {
     return (
@@ -11,6 +15,8 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/products" element={<Products />} />
+                <Route path="/cart" element={<Cart />} />
+                {/*<Route path="/orders" element={<Orders />} />*/}
             </Routes>
         </BrowserRouter>
     );
